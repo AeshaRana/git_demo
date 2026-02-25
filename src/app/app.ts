d35206ea0dc9c911aeb833881a10fa6c4cfa8064
aesha <aesharaapyd@gmail.com>
2026-02-25 14:53:34 +0530

    modified app.html and app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component/navbar.component";
+import { SidebarComponent } from "./sidebar/sidebar.component/sidebar.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
