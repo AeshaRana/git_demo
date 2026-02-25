3add90567afb183e45c61d0e94a4c0f8af5b2bc5
aesha <aesharaapyd@gmail.com>
2026-02-25 15:09:26 +0530

    dummy component added

---
+++ src/app/dummy.component/dummy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dummy.component',
+  imports: [],
+  templateUrl: './dummy.component.html',
+  styleUrl: './dummy.component.scss',
+})
+export class DummyComponent {
+
+}
